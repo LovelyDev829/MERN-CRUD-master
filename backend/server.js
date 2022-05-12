35f9a442566883b922ee28ef755677a5b9712b4a
LovelyDev829 <lovelydeveloper829@gmail.com>
2022-05-12 08:40:32 +0300

    MERN full-stack crud-master.

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+let express = require('express')
+let mongoose = require('mongoose')
+let cors = require('cors')
+let bodyParser = require('body-parser')
+let dbConfig = require('./database/db')
+
+// Express Route
+const studentRoute = require('../backend/routes/student.route')
+
+// Connecting mongoDB Database
+mongoose.Promise = global.Promise
+mongoose
+  .connect(dbConfig.db, {
+    useNewUrlParser: true,
+  })
+  .then(
+    () => {
+      console.log('Database sucessfully connected!')
+    },
+    (error) => {
+      console.log('Could not connect to database : ' + error)
+    },
+  )
+
+const app = express()
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+)
+app.use(cors())
+app.use('/students', studentRoute)
+
+// PORT
+const port = process.env.PORT || 4000
+const server = app.listen(port, () => {
+  console.log('Connected to port ' + port)
+})
+
+// 404 Error
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err.message)
+  if (!err.statusCode) err.statusCode = 500
+  res.status(err.statusCode).send(err.message)
+})
